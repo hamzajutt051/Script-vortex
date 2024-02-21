2790518891deb0c526e48a572f85427d83054235
hamzajutt506 <sp19-bcs-051@cuilahore.edu.pk>
2024-02-21 17:10:02 +0500

    main

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,20 +1,6 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-import rating from "../../assets/images/Rating.png";
-import { TiTick } from "react-icons/ti";
-import { FaShippingFast } from "react-icons/fa";
-import { FaRegSmile } from "react-icons/fa";
-import { MdOutlineSecurity } from "react-icons/md";
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 import "./home.css";
-import useCart from "../../hook/useCart";
-import Rating from "react-rating";
-import { MdOutlineStarOutline, MdOutlineStarPurple500 } from "react-icons/md";
-import { MdOutlineCancel } from "react-icons/md";
-import useWindowDimensions from "../../hook/useWindowDimensions";
-import { FaArrowUp } from "react-icons/fa";
+
 import laptop from "../../assets/images/laptop.png";
 
 export default function HomePage() {
