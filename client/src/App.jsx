e9b2db66ad8fc24561ec2ae2ce7284372a96a374
hamzajutt506 <sp19-bcs-051@cuilahore.edu.pk>
2024-02-21 16:56:37 +0500

    main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,11 @@ import { Toaster } from "react-hot-toast";
 import React from "react";
 
 import AppRoutes from "./routes";
-
-import "react-advanced-cropper/dist/style.css";
-import "./config/axios";
-
-import LoadingProvider from "./context/loading";
-import AuthProvider from "./context/auth";
-import CartProvider from "./context/cart";
-
 export default function App() {
   return (
-    <LoadingProvider>
-      <AuthProvider>
-        <CartProvider>
-          <AppRoutes />
-          <Toaster />
-        </CartProvider>
-      </AuthProvider>
-    </LoadingProvider>
+    <>
+      <AppRoutes />
+      <Toaster />
+    </>
   );
 }
--- client/src/utils/prices.js
+++
@@ -1,4 +0,0 @@
-export const prices = [
-  { frame: 9, price: 100, freeDelivery: false },
-  { frame: 12, price: 120, freeDelivery: true },
-];
